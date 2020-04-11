4f207e8958730d961cc1543f5d744d3ece541f7a
Wintersolid <ztanksley@luc.edu>
2020-04-11 09:14:16 -0500

    update

--- Wargame2020/src/Ship.java
+++ Wargame2020/src/Ship.java
@@ -15,3 +15,24 @@ public class Ship
     private int length;
 
     private int direction;
+    private int direction;
+
+
+
+    //Direction constants
+
+    public static final int UNSET = -1;
+
+    public static final int HORIZONTAL = 0;
+
+    public static final int VERTICAL = 1;
+
+
+
+/*This is the Ship constructor, it will create a ship of a
+
+ * given length with a default location of (-1, -1) --which
+
+ * will not be on the grid-- and an unset
+
+ * direction @param length = length of ship*/
