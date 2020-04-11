55a93e171a016e229158ada32b2a1665d9d63126
Wintersolid <ztanksley@luc.edu>
2020-04-11 09:18:29 -0500

    update

--- Wargame2020/src/Ship.java
+++ Wargame2020/src/Ship.java
@@ -1,6 +1,6 @@
 /*This class holds the values of a ship on the Grid.
 
- * Specifically it keeps track of the length, coordinate
+ * keeps track of the length, coordinate
 
  * of initial spot, and direction of the ship*/
 
@@ -15,3 +15,24 @@ public class Ship
     private int length;
 
     private int direction;
+    private int direction;
+
+
+
+    //Direction constants
+
+    public static final int UNSET = -1;
+
+    public static final int HORIZONTAL = 0;
+
+    public static final int VERTICAL = 1;
+
+
+
+/*This is the Ship constructor, it will create a ship of a
+
+ * given length with a default location of (-1, -1) --which
+
+ * will not be on the grid-- and an unset
+
+ * direction @param length = length of ship*/
