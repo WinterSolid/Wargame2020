ebab875a7eecc3ea90471c6348a49caa5338e8ce
ijehan <sjehan@luc.edu>
2020-04-14 22:10:54 -0500

    check
    Signed-off-by: ijehan <sjehan@luc.edu>

--- Wargame2020/src/Randomizer.java
+++ Wargame2020/src/Randomizer.java
@@ -1,2 +1,25 @@
-public class Randomizer {
-}
+/**
+
+ * Class made for obtaining random values for Battleship game
+
+ */
+
+import java.util.*;
+
+
+
+public class Randomizer{
+
+
+
+    public static Random theInstance = null;
+
+
+
+    public Randomizer(){
+
+    }
+
+
+
+    }
