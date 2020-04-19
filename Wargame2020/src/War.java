843be28fd15cd514cc2a23347b0b1241af88b2aa
ijehan <sjehan@luc.edu>
2020-04-18 20:53:09 -0500

    war method
    Signed-off-by: ijehan <sjehan@luc.edu>

--- Wargame2020/src/War.java
+++ Wargame2020/src/War.java
@@ -1,2 +1,21 @@
+
+
+/**
+
+ * This is a simple class used to run the game
+
+ */
+
 public class War {
+
+    public static void main(String[] args) {
+
+        BattleShip bs = new BattleShip();
+
+        bs.run();
+
+    }
+
 }
+
+
